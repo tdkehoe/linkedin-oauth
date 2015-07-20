9e4fc5d6a337cebc3c3b56e32555173fb4312d47
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-07-20 14:02:16 -0600

    Somewhere...

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var passport = require('passport');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,23 +25,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 
+// passport.use(new LinkedInStrategy({
+//   clientID: process.LINKEDIN_CLIENT_ID,
+//   clientSecret: process.LINKEDIN_CLIENT_SECRET,
+//   callbackURL: "http://localhost:3000/auth/linkedin/callback",
+//   scope: ['r_emailaddress', 'r_basicprofile'],
+// }, function(accessToken, refreshToken, profile, done) {
+//   // asynchronous verification, for effect...
+//   process.nextTick(function () {
+//     // To keep the example simple, the user's LinkedIn profile is returned to
+//     // represent the logged-in user. In a typical application, you would want
+//     // to associate the LinkedIn account with a user record in your database,
+//     // and return that user instead.
+//     return done(null, profile);
+//   });
+// }));
+
+// below app.use(passport.session());...
 passport.use(new LinkedInStrategy({
-  clientID: '78ahzm1auahdt6',
-  clientSecret: 'wpUKDXWeOmkINkig',
-  callbackURL: "http://localhost:3000/auth/linkedin/callback",
-  scope: ['r_emailaddress', 'r_basicprofile'],
-}, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
-}));
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/linkedin/callback",
+    scope: ['r_emailaddress', 'r_basicprofile']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
+  }
+));
 
 app.get('/auth/linkedin',
   passport.authenticate('linkedin', { state: 'SOME STATE'  }),
@@ -49,11 +63,18 @@ app.get('/auth/linkedin',
     // function will not be called.
   });
 
-// neccesary?
-// app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-//   successRedirect: '/',
-//   failureRedirect: '/login'
-// }));
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.use('/', routes);
 app.use('/users', users);
